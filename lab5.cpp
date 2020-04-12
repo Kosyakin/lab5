d9d84dc3b47e6bbc77e48b20330291dafa4e0678
Kosyakin <62693680+Kosyakin@users.noreply.github.com>
2020-04-12 22:24:46 +0300

    Update lab5.cpp

--- lab5.cpp
+++ lab5.cpp
@@ -3,48 +3,46 @@
 //Если для вычисления интеграла Вам требуется его как - то преобразовать(например, сделать замену или разложить 
 //в ряд подынтегральную функцию), опишите это в файле Readme.md(формат markdown), желательно с формулами.
 
+//У тех, у кого в билете функция задана таблицей, требуется сначала построить кубический сплайн,
+//и интегрировать уже его с каким - то малым шагом, контролируя погрешность по методу Рунге.
+//Если для вычисления интеграла Вам требуется его как - то преобразовать(например, сделать замену или разложить 
+//в ряд подынтегральную функцию), опишите это в файле Readme.md(формат markdown), желательно с формулами.
+
 
 #include "pch.h"
 #include <iostream>
 using namespace std;
 
-// Функция вычисления определенного интеграла
-double Integral(double *f, double step) {
-	double value = 0;
-	for (int i = 0; i < NUMPOINT; i++) {
-		value += f[i];
-	}
-	value *= step;
-	return value;
-}
-int main() {
-	double *f;
-	double step, t;
-	double S;
-	int i;
-	system("chcp 1251");
-	system("cls");
-	f = (double*)malloc(NUMPOINT * sizeof(double));
-	printf("Количество точек = %d\n", NUMPOINT);
-	step = PI / NUMPOINT; // величина шага (высота трапеций)
-	printf("Величина шага = %lf\n", step);
-	t = 0.0;
-	// Инициализация значений функции f(t)=sin(t)
-	for (i = 0; i < NUMPOINT; i++) {
-		f[i] = sin(t);
-		t += step;
-	}
-	S = Integral(f, step); // вычисление интеграла
-	printf("Значение интеграла = %lf", S);
-	getchar();
-	return 0;
-}
+//Метод трапеций для таблицы с шагом 0.25
 
-
+void metodtrapec(int n,  double shag, double *F )
+{
+	double s = 0;
+	for (int i = 1;i < n;i++) {
+		s += ((shag)*(F[i] + F[i - 1]) / 2);
+		//cout << "integral=" << s << endl;
+	}
+	cout <<endl<< "integral=" << s << endl;
+}
 
 
 int main()
 {
-	int n = 10;//количество столбцов
+	int n = 9;			//количество столбцов
+	double *x, *F, shag;
+	shag = 0.25;
+	x = new double[n];
+	F = new double[n];
+	F[0] = 0;		F[1] = 0.028;	F[2] = 0.054;
+	F[3] = 0.078;	F[4] = 0.1;		F[5] = 0.2;
+	F[6] = 0.133;	F[7] = 0.145;	F[8] = 0.154;
+	double c = 0;
+	for (int i = 0;c < n;c++) {
+		x[i] = c / 4;
+		cout << x[i]<<" | "<<F[i] << endl;
+	}
 
+	metodtrapec(n, shag, F);
+
+	system("pause");
 }
